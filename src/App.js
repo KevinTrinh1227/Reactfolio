da46383309bdefe4f9949dd5d778ff659790ea83
Kevin Huy Trinh <48145892+KevinTrinh1227@users.noreply.github.com>
2023-07-19 00:22:37 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Test
+          Edit <code>src/App.js</code> Testing
         </p>
         <a
           className="App-link"
