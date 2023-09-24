af12974762af8443babdc22753fb0c47fc15d419
KevinTrinh1227 <kevintrinh1227@gmail.com>
2023-09-24 01:54:56 -0500

    Minor updates

--- src/components/sections/WorkExperience.js
+++ src/components/sections/WorkExperience.js
@@ -14,43 +14,53 @@ const WorkExperience = () => {
   return (
     <section className="Experience-section">
       <div className="container">
-          <div className="row">
-            <div className="col-sm-12">
-              <div className="title-box text-center">
-                <h3 className="title-a" id="experience">
-                  {experience.section.title}
-                  <div className="line-mf"></div>
-                </h3>
-                <p className="subtitle-a">{experience.section.description}</p>
-              </div>
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="title-box text-center">
+              <h3 className="title-a" id="experience">
+                {experience.section.title}
+                <div className="line-mf"></div>
+              </h3>
+              <p className="subtitle-a">{experience.section.description}</p>
             </div>
           </div>
+        </div>
 
-          <div className="col-sm-12">
-              <div className="wrapper">
-                <ul className="indicator">
-                  {experienceItems.map((job, index) => (
-                    <li
-                      key={index}
-                      className={index === activeJobIndex ? "active" : ""}
-                      onClick={() => handleTabClick(index)}
-                      data-target={job.organization.split(" ").join("-")}
-                    >
-                      {job.organization}
+        <div className="col-sm-12">
+          <div className="wrapper">
+            <ul className="indicator">
+              {experienceItems.map((job, index) => (
+                <li
+                  key={index}
+                  className={index === activeJobIndex ? "active" : ""}
+                  onClick={() => handleTabClick(index)}
+                  data-target={job.organization.split(" ").join("-")}
+                >
+                  {job.organization}
+                </li>
+              ))}
+            </ul>
+            <div className="content">
+              <h1>
+                {experienceItems[activeJobIndex].title} @{" "}
+                {experienceItems[activeJobIndex].organization}
+              </h1>
+              <h4>
+                {experienceItems[activeJobIndex].start_date} -{" "}
+                {experienceItems[activeJobIndex].end_date}
+              </h4>
+              <ul>
+                {experienceItems[activeJobIndex].description.map(
+                  (paragraph, paraIndex) => (
+                    <li className="active" key={paraIndex}>
+                      {paragraph}
                     </li>
-                  ))}
-                </ul>
-                <div className="content">
-                  <h1>{experienceItems[activeJobIndex].title} @ {experienceItems[activeJobIndex].organization}</h1>
-                  <h4>{experienceItems[activeJobIndex].start_date} - {experienceItems[activeJobIndex].end_date}</h4>
-                  <ul>
-                    {experienceItems[activeJobIndex].description.map((paragraph, paraIndex) => (
-                      <li className="active" key={paraIndex}>{paragraph}</li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
+                  )
+                )}
+              </ul>
+            </div>
           </div>
+        </div>
       </div>
     </section>
   );
