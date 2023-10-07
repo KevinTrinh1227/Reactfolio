eb7112d1e45d353dcd2a6af40e44d41e6f59e3cf
KevinTrinh1227 <kevintrinh1227@gmail.com>
2023-10-07 00:04:56 -0500

    Added API usage to footer with repo stars and forks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import contentData from "../content.json";
 import { Fade } from "react-awesome-reveal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCodeFork, faStar } from "@fortawesome/free-solid-svg-icons";
 
 /* ==========================================
 *   JSON Template Example
@@ -18,11 +20,32 @@ import { Fade } from "react-awesome-reveal";
 
 */
 
+async function fetchRepoData(repoApiLink) {
+  const response = await fetch(repoApiLink);
+  const data = await response.json();
+  return data;
+}
+
 const Footer = () => {
   const general = contentData.general;
+  const repoLink = contentData.repo_stats.repo_link;
+  const repoApiLink = contentData.repo_stats.api_link;
+
+  const iconStyle = {
+    fontSize: "0.8rem", // Adjust the size as needed
+    marginRight: "1.2rem", // Add space between icon and text
+  };
+
+  const [repoData, setRepoData] = useState(null);
+
+  useEffect(() => {
+    fetchRepoData(repoApiLink).then((data) => {
+      setRepoData(data);
+    });
+  }, [repoApiLink]);
 
   return (
-    <Fade triggerOnce={true}>
+    <Fade triggerOnce={true} direction="up">
       <footer>
         <div className="container">
           <div className="row">
@@ -40,6 +63,16 @@ const Footer = () => {
                 </p>
                 <div className="credits">
                   {contentData.footer.copyright_line} &copy;
+                </div>
+                <div className="fork-stars">
+                  <a href={repoLink}>
+                    <span>
+                      {repoData && repoData.forks_count}{" "}
+                      <FontAwesomeIcon icon={faCodeFork} style={iconStyle} />
+                      {repoData && repoData.stargazers_count}{" "}
+                      <FontAwesomeIcon icon={faStar} style={iconStyle} />
+                    </span>
+                  </a>
                 </div>
               </div>
             </div>
